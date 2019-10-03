3f01e24745d4df584c8eeb4070c167125816b160
UVADave <dpg3k@virginia.edu>
2019-10-03 08:51:56 -0400

    Config by environment. Use worker model

--- cmd/virgo4-simple-ingest/config.go
+++ cmd/virgo4-simple-ingest/config.go
@@ -1,40 +1,76 @@
 package main
 
 import (
-	"flag"
 	"log"
+	"os"
+	"strconv"
 )
 
 // ServiceConfig defines all of the service configuration parameters
 type ServiceConfig struct {
-	OutQueueName  string
-	FileName      string
-	MaxCount      uint
+	OutQueueName      string   // SQS queue name for outbound documents
+
+	MessageBucketName string   // the bucket to use for large messages
+
+	DataSourceName    string   // the name to associate the data with. Each record has metadata showing this value
+	FileName          string   // the input file name
+	MaxCount          uint     // the maximum document count to ingest, 0 is no limit
+
+	WorkerQueueSize   int      // the inbound message queue size to feed the workers
+	Workers           int      // the number of worker processes
+}
+
+func ensureSet(env string) string {
+	val, set := os.LookupEnv(env)
+
+	if set == false {
+		log.Printf("environment variable not set: [%s]", env)
+		os.Exit(1)
+	}
+
+	return val
+}
+
+func ensureSetAndNonEmpty(env string) string {
+	val := ensureSet(env)
+
+	if val == "" {
+		log.Printf("environment variable not set: [%s]", env)
+		os.Exit(1)
+	}
+
+	return val
+}
+
+func envToInt( env string ) int {
+
+	number := ensureSetAndNonEmpty( env )
+	n, err := strconv.Atoi( number )
+	fatalIfError( err )
+	return n
 }
 
 // LoadConfiguration will load the service configuration from env/cmdline
 // and return a pointer to it. Any failures are fatal.
 func LoadConfiguration() *ServiceConfig {
 
-	log.Printf("Loading configuration...")
 	var cfg ServiceConfig
-	flag.StringVar(&cfg.OutQueueName, "outqueue", "", "Outbound queue name")
-	flag.StringVar(&cfg.FileName, "infile", "", "Batch file")
-	flag.UintVar(&cfg.MaxCount, "max", 0, "Maximum number of records to ingest (0 is all of them)")
 
-	flag.Parse()
-
-	if len( cfg.OutQueueName ) == 0 {
-		log.Fatalf( "OutQueueName cannot be blank" )
-	}
-
-	if len( cfg.FileName ) == 0 {
-		log.Fatalf( "FileName cannot be blank" )
-	}
+	cfg.OutQueueName = ensureSetAndNonEmpty( "VIRGO4_SIMPLE_INGEST_OUT_QUEUE" )
+	cfg.MessageBucketName = ensureSetAndNonEmpty( "VIRGO4_SQS_MESSAGE_BUCKET" )
+	cfg.DataSourceName = ensureSetAndNonEmpty( "VIRGO4_SIMPLE_INGEST_DATA_SOURCE" )
+	cfg.MaxCount = uint( envToInt( "VIRGO4_SIMPLE_INGEST_MAX_COUNT" ) )
+	cfg.FileName = ensureSetAndNonEmpty( "VIRGO4_SIMPLE_INGEST_FILE_NAME" )
+	cfg.WorkerQueueSize = envToInt( "VIRGO4_SIMPLE_INGEST_WORK_QUEUE_SIZE" )
+	cfg.Workers = envToInt( "VIRGO4_SIMPLE_INGEST_WORKERS" )
 
 	log.Printf("[CONFIG] OutQueueName         = [%s]", cfg.OutQueueName )
+	log.Printf("[CONFIG] DataSourceName       = [%s]", cfg.DataSourceName )
+	log.Printf("[CONFIG] MessageBucketName    = [%s]", cfg.MessageBucketName )
 	log.Printf("[CONFIG] FileName             = [%s]", cfg.FileName )
 	log.Printf("[CONFIG] MaxCount             = [%d]", cfg.MaxCount )
+	log.Printf("[CONFIG] WorkerQueueSize      = [%d]", cfg.WorkerQueueSize )
+	log.Printf("[CONFIG] Workers              = [%d]", cfg.Workers )
 
 	return &cfg
 }
--- cmd/virgo4-simple-ingest/main.go
+++ cmd/virgo4-simple-ingest/main.go
@@ -15,31 +15,32 @@ import (
 //
 func main() {
 
-	log.Printf("===> %s service staring up <===", os.Args[ 0 ] )
+	log.Printf("===> %s service staring up (version: %s) <===", os.Args[ 0 ], Version( ) )
 
 	// Get config params and use them to init service context. Any issues are fatal
 	cfg := LoadConfiguration()
 
 	// load our AWS_SQS helper object
-	aws, err := awssqs.NewAwsSqs( awssqs.AwsSqsConfig{ } )
-	if err != nil {
-		log.Fatal( err )
-	}
+	aws, err := awssqs.NewAwsSqs( awssqs.AwsSqsConfig{ MessageBucketName: cfg.MessageBucketName } )
+	fatalIfError( err )
 
 	// get the queue handle from the queue name
 	outQueueHandle, err := aws.QueueHandle( cfg.OutQueueName )
-	if err != nil {
-		log.Fatal( err )
+	fatalIfError( err )
+
+	// create the record channel
+	outboundMessageChan := make( chan awssqs.Message, cfg.WorkerQueueSize )
+
+	// start workers here
+	for w := 1; w <= cfg.Workers; w++ {
+		go worker( w, cfg, aws, outboundMessageChan, outQueueHandle )
 	}
 
 	file, err := os.Open( cfg.FileName )
-	if err != nil {
-		log.Fatal( err )
-	}
+	fatalIfError( err )
 	defer file.Close( )
 
 	reader := bufio.NewReader( file )
-	block := make( []string, 0, awssqs.MAX_SQS_BLOCK_COUNT )
 
 	count := uint( 0 )
 	start := time.Now()
@@ -53,32 +54,20 @@ func main() {
 			if err == io.EOF {
 				break
 			} else {
-				log.Fatal(err)
+				fatalIfError( err )
 			}
 		}
 
-		sz := uint( len( line ) )
-		if sz >= awssqs.MAX_SQS_MESSAGE_SIZE {
-			log.Printf("Ignoring record %d as too large (%d characters)", count, sz )
-			continue
-		}
+		//sz := uint( len( line ) )
+		//if sz >= awssqs.MAX_SQS_MESSAGE_SIZE {
+		//	log.Printf("Ignoring record %d as too large (%d characters)", count, sz )
+		//	continue
+		//}
 
 		count ++
-    	block = append(block, line )
+		outboundMessageChan <- constructMessage( cfg.DataSourceName, awssqs.Payload( line ) )
 
-		// have we reached a block size limit
-		if count % awssqs.MAX_SQS_BLOCK_COUNT == awssqs.MAX_SQS_BLOCK_COUNT - 1 {
-
-			err := sendMessages( cfg, aws, outQueueHandle, block)
-			if err != nil {
-				log.Fatal( err )
-			}
-
-			// reset the block
-			block = block[:0]
-		}
-
-		if count % 100 == 0 {
+		if count % 1000 == 0 {
 			duration := time.Since(start)
 			log.Printf("Processed %d records (%0.2f tps)", count, float64( count ) / duration.Seconds() )
 		}
@@ -89,52 +78,26 @@ func main() {
 		}
 	}
 
-	// any remaining records?
-	if len(block) != 0 {
-
-		err := sendMessages( cfg, aws, outQueueHandle, block)
-		if err != nil {
-			log.Fatal( err )
-		}
-	}
-
 	duration := time.Since(start)
 	log.Printf("Done, processed %d records in %0.2f seconds (%0.2f tps)", count, duration.Seconds(), float64( count ) / duration.Seconds() )
-}
 
-func sendMessages( cfg * ServiceConfig, aws awssqs.AWS_SQS, queue awssqs.QueueHandle, messages []string) error {
-
-	count := len( messages )
-	if count == 0 {
-		return nil
-	}
-	batch := make( []awssqs.Message, 0, count )
-	for _, m := range messages {
-		batch = append( batch, constructMessage( cfg.FileName, m ) )
-	}
-
-	opStatus, err := aws.BatchMessagePut( queue, batch )
-	if err != nil {
-		return err
-	}
-
-	// check the operation results
-	for ix, op := range opStatus {
-		if op == false {
-			log.Printf( "WARNING: message %d failed to send to outbound queue", ix )
+	for {
+		if len( outboundMessageChan ) == 0 {
+			break
 		}
+		log.Printf("Waiting for workers to complete... zzzz" )
+		time.Sleep( 1 * time.Second)
 	}
-
-	return nil
 }
 
-func constructMessage( filename string, message string ) awssqs.Message {
+func constructMessage( datasource string, message awssqs.Payload ) awssqs.Message {
 
-	attributes := make( []awssqs.Attribute, 0, 3 )
-	attributes = append( attributes, awssqs.Attribute{ "op", "add" } )
-	attributes = append( attributes, awssqs.Attribute{ "src", filename } )
+	attributes := make( []awssqs.Attribute, 0, 2 )
+	//attributes = append( attributes, awssqs.Attribute{ "op", "add" } )
+	//attributes = append( attributes, awssqs.Attribute{ "src", filename } )
 	attributes = append( attributes, awssqs.Attribute{ "type", "xml"} )
-	return awssqs.Message{ Attribs: attributes, Payload: awssqs.Payload( message )}
+	attributes = append( attributes, awssqs.Attribute{ Name: "source", Value: datasource } )
+	return awssqs.Message{ Attribs: attributes, Payload: message }
 }
 
 //
