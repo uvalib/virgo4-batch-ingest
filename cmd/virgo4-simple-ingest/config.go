cd36e6ac8e5ef59134529377b561af261bfaf200
UVADave <dpg3k@virginia.edu>
2019-10-17 10:24:50 -0400

    Latest SDK and a go fmt

--- cmd/virgo4-simple-ingest/config.go
+++ cmd/virgo4-simple-ingest/config.go
@@ -8,16 +8,16 @@ import (
 
 // ServiceConfig defines all of the service configuration parameters
 type ServiceConfig struct {
-	OutQueueName      string   // SQS queue name for outbound documents
+	OutQueueName string // SQS queue name for outbound documents
 
-	MessageBucketName string   // the bucket to use for large messages
+	MessageBucketName string // the bucket to use for large messages
 
-	DataSourceName    string   // the name to associate the data with. Each record has metadata showing this value
-	FileName          string   // the input file name
-	MaxCount          uint     // the maximum document count to ingest, 0 is no limit
+	DataSourceName string // the name to associate the data with. Each record has metadata showing this value
+	FileName       string // the input file name
+	MaxCount       uint   // the maximum document count to ingest, 0 is no limit
 
-	WorkerQueueSize   int      // the inbound message queue size to feed the workers
-	Workers           int      // the number of worker processes
+	WorkerQueueSize int // the inbound message queue size to feed the workers
+	Workers         int // the number of worker processes
 }
 
 func ensureSet(env string) string {
@@ -42,11 +42,11 @@ func ensureSetAndNonEmpty(env string) string {
 	return val
 }
 
-func envToInt( env string ) int {
+func envToInt(env string) int {
 
-	number := ensureSetAndNonEmpty( env )
-	n, err := strconv.Atoi( number )
-	fatalIfError( err )
+	number := ensureSetAndNonEmpty(env)
+	n, err := strconv.Atoi(number)
+	fatalIfError(err)
 	return n
 }
 
@@ -56,21 +56,21 @@ func LoadConfiguration() *ServiceConfig {
 
 	var cfg ServiceConfig
 
-	cfg.OutQueueName = ensureSetAndNonEmpty( "VIRGO4_SIMPLE_INGEST_OUT_QUEUE" )
-	cfg.MessageBucketName = ensureSetAndNonEmpty( "VIRGO4_SQS_MESSAGE_BUCKET" )
-	cfg.DataSourceName = ensureSetAndNonEmpty( "VIRGO4_SIMPLE_INGEST_DATA_SOURCE" )
-	cfg.MaxCount = uint( envToInt( "VIRGO4_SIMPLE_INGEST_MAX_COUNT" ) )
-	cfg.FileName = ensureSetAndNonEmpty( "VIRGO4_SIMPLE_INGEST_FILE_NAME" )
-	cfg.WorkerQueueSize = envToInt( "VIRGO4_SIMPLE_INGEST_WORK_QUEUE_SIZE" )
-	cfg.Workers = envToInt( "VIRGO4_SIMPLE_INGEST_WORKERS" )
+	cfg.OutQueueName = ensureSetAndNonEmpty("VIRGO4_SIMPLE_INGEST_OUT_QUEUE")
+	cfg.MessageBucketName = ensureSetAndNonEmpty("VIRGO4_SQS_MESSAGE_BUCKET")
+	cfg.DataSourceName = ensureSetAndNonEmpty("VIRGO4_SIMPLE_INGEST_DATA_SOURCE")
+	cfg.MaxCount = uint(envToInt("VIRGO4_SIMPLE_INGEST_MAX_COUNT"))
+	cfg.FileName = ensureSetAndNonEmpty("VIRGO4_SIMPLE_INGEST_FILE_NAME")
+	cfg.WorkerQueueSize = envToInt("VIRGO4_SIMPLE_INGEST_WORK_QUEUE_SIZE")
+	cfg.Workers = envToInt("VIRGO4_SIMPLE_INGEST_WORKERS")
 
-	log.Printf("[CONFIG] OutQueueName         = [%s]", cfg.OutQueueName )
-	log.Printf("[CONFIG] DataSourceName       = [%s]", cfg.DataSourceName )
-	log.Printf("[CONFIG] MessageBucketName    = [%s]", cfg.MessageBucketName )
-	log.Printf("[CONFIG] FileName             = [%s]", cfg.FileName )
-	log.Printf("[CONFIG] MaxCount             = [%d]", cfg.MaxCount )
-	log.Printf("[CONFIG] WorkerQueueSize      = [%d]", cfg.WorkerQueueSize )
-	log.Printf("[CONFIG] Workers              = [%d]", cfg.Workers )
+	log.Printf("[CONFIG] OutQueueName         = [%s]", cfg.OutQueueName)
+	log.Printf("[CONFIG] DataSourceName       = [%s]", cfg.DataSourceName)
+	log.Printf("[CONFIG] MessageBucketName    = [%s]", cfg.MessageBucketName)
+	log.Printf("[CONFIG] FileName             = [%s]", cfg.FileName)
+	log.Printf("[CONFIG] MaxCount             = [%d]", cfg.MaxCount)
+	log.Printf("[CONFIG] WorkerQueueSize      = [%d]", cfg.WorkerQueueSize)
+	log.Printf("[CONFIG] Workers              = [%d]", cfg.Workers)
 
 	return &cfg
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/uvalib/virgo4-simple-ingest
 go 1.13
 
 require (
-	github.com/antchfx/xmlquery v1.0.0 // indirect
-	github.com/antchfx/xpath v1.0.0 // indirect
-	github.com/aws/aws-sdk-go v1.25.5 // indirect
-	github.com/uvalib/virgo4-sqs-sdk/awssqs v0.0.0-20191004120004-aae172a17c2d
+	github.com/antchfx/xmlquery v1.1.0
+	github.com/antchfx/xpath v1.1.0 // indirect
+	github.com/aws/aws-sdk-go v1.25.14 // indirect
+	github.com/uvalib/virgo4-sqs-sdk/awssqs v0.0.0-20191017135820-73d89205de18
+	golang.org/x/net v0.0.0-20191014212845-da9a3fd4c582 // indirect
+	golang.org/x/text v0.3.2 // indirect
 )
